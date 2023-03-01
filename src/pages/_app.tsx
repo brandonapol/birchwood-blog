4108410d173e49918f90872ad826e64c683ce1e8
Brandon Apol <brandonapol@cedarville.edu>
2023-02-28 22:21:40 -0500

    landing page working

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,9 +9,11 @@ const MyApp: AppType<{ session: Session | null }> = ({
   pageProps: { session, ...pageProps },
 }) => {
   return (
+    <>
     <SessionProvider session={session}>
       <Component {...pageProps} />
     </SessionProvider>
+    </>
   );
 };
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,15 @@
 const config = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'timberwolf': '#E6E1DB',
+        'payne': '#536373',
+        'slate-grey': '#7F8894',
+        'cadet': '#9FACBD',
+        'linen': '#FAEFE4'
+      }
+    },
   },
   plugins: [],
 };

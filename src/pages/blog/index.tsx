e82d0e2ff6bcb78864154f1bded34864bb4e8485
Brandon Apol <brandonapol@cedarville.edu>
2023-12-26 21:27:48 -0500

    fixed api return types

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,69 +1,71 @@
-import BlogLoop, { Blog } from '~/components/BlogLoop';
-import Link from 'next/link';
+import BlogLoop, { Blog } from "~/components/BlogLoop";
+import Link from "next/link";
 
 type BlogProps = {
-  final: object
-}
+  returnedData: object;
+};
 
-const BlogMain = ({ final }: BlogProps ) => {
-
-  const finalObj= Object.values(final);
+const BlogMain = ({ returnedData }: BlogProps) => {
+  const finalObj = Object.values(returnedData);
   // TODO - load a new page on click for each blog post
   return (
-  <div>
-    
-    <div className='flex bg-cadet p-3'>
-      <Link href='/' className='border-lg flex hover:animate-pulse justify-center align-middle items-center p-3
-     transition duration-500 border-white border-2 rounded hover:bg-slate-700
-      hover:text-timberwolf'>
-        <div className="p-3 text-xl">&#11013;</div>
-        Back to Portfolio
-      </Link></div>
-    <div className='flex flex-col justify-center items-center align-middle p-5'>
-      {
-        finalObj.map((obj: Blog) => (
+    <div>
+      <div className="flex bg-cadet p-3">
+        <Link
+          href="/"
+          className="border-lg flex items-center justify-center rounded border-2 border-white
+     p-3 align-middle transition duration-500 hover:animate-pulse hover:bg-slate-700
+      hover:text-timberwolf"
+        >
+          <div className="p-3 text-xl">&#11013;</div>
+          Back to Portfolio
+        </Link>
+      </div>
+      <div className="flex flex-col items-center justify-center p-5 align-middle">
+        {finalObj.map((obj: Blog) => (
           <BlogLoop key={obj.title} {...obj} />
-        ))
-      }
+        ))}
+      </div>
     </div>
-  </div> )
-}
+  );
+};
 //! you will need to set this as async
 export async function getStaticProps() {
+  let returnedData: Blog;
   if (!process.env.NEXT_PUBLIC_API_URL) {
-    throw new Error('Cannot find API url')
+    throw new Error("Cannot find API url");
   }
-  const apiUrl: string = process.env.NEXT_PUBLIC_API_URL
-  if (apiUrl.includes('localhost')) {
-    const final: Blog = {
-      "title": "string",
-      "author": "string",
-      "content": "string",
-      "createdAt": "string",
-      "updatedAt": "string",
-    }
-    return final;
-  }
-  const response = await fetch(`${apiUrl}/api/blog`,
-    {
-      method: 'GET',
+  const apiUrl: string = process.env.NEXT_PUBLIC_API_URL;
+  if (!apiUrl.includes("localhost")) {
+    const response = await fetch(`${apiUrl}/api/blog`, {
+      method: "GET",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
     });
 
     if (!response.ok) {
-      throw new Error('Unable to get data')
+      throw new Error("Unable to get data");
     }
-
-    const final: Blog = await response.json() as Blog;
-
 
     return {
-      props: { 
-        final,
+      props: {
+        returnedData: (await response.json()) as Blog,
       },
-    }
+    };
+  } else {
+    return {
+      props: {
+        returnedData: {
+          title: "string",
+          author: "string",
+          content: "string",
+          createdAt: "string",
+          updatedAt: "string",
+        },
+      },
+    };
+  }
 }
 
-export default BlogMain
+export default BlogMain;
